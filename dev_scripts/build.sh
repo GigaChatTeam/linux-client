d6951857af9c323b81a5992c79bebabe34f32556
MrSvinia <i@savnil.ru>
2024-08-10 19:51:35 +0300

    Cleanup repository

--- dev_scripts/build.sh
+++
@@ -1,7 +0,0 @@
-cd build
-cmake ..
-cp compile_commands.json ..
-cd ..
-dev_scripts/fix_compile_commands.sh
-cd build
-make -j24
--- dev_scripts/fix_compile_commands.sh
+++
@@ -1,2 +0,0 @@
-sed -i 's/-mno-direct-extern-access //g' compile_commands.json
-sed -i 's/gnu++23/gnu++2b/g' compile_commands.json
